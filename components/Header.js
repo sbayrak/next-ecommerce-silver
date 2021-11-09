0fff77652b255cfd22bae248e624a864927f4b29
erayhacioglu <hacoglueray@gmail.com>
2021-11-09 22:18:48 +0300

    preliminary

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<div>
+			<h1 className='text-center'>Header</h1>
+		</div>
+	);
+};
+
+export default Header;
---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
+						rel='stylesheet'
+					/>
+					<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'></script>
+					<link
+						rel='stylesheet'
+						href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css'
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
