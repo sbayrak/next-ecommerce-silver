a6d4ec905cd08822083502da3e54444139650360
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-11-13 17:04:26 +0200

    working on carousel v2 + mobile is done

--- components/reuseable/Carousel.js
+++ components/reuseable/Carousel.js
@@ -1,19 +1,45 @@
+import Image from 'next/image';
+
 const Carousel = () => {
   return (
     <div
       id='carouselExampleControls'
-      className='carousel slide'
+      className='carousel slide  '
       data-bs-ride='carousel'
     >
       <div className='carousel-inner'>
-        <div className='carousel-item active'>
-          <img src='/compressed.jpg' className='d-block w-100' alt='asd' />
+        <div
+          className='carousel-item active carousel-item2 '
+          data-bs-interval='30000'
+        >
+          <a href='#!' className='carousel-link'>
+            <Image
+              src='/compressed.jpg'
+              className='d-block w-100  '
+              alt='asd'
+              layout='fill'
+            />
+          </a>
         </div>
-        <div className='carousel-item'>
-          <img src='/compressed.jpg' className='d-block w-100' alt='asd' />
+        <div className='carousel-item carousel-item2' data-bs-interval='3000'>
+          <a href='#!' className='carousel-link'>
+            <Image
+              src='/compressed.jpg'
+              className='d-block w-100   '
+              alt='asd'
+              layout='fill'
+            />
+          </a>
         </div>
-        <div className='carousel-item'>
-          <img src='/compressed.jpg' className='d-block w-100' alt='asd' />
+        <div className='carousel-item carousel-item2' data-bs-interval='3000'>
+          <a href='#!' className='carousel-link'>
+            <Image
+              src='/compressed.jpg'
+              className='d-block w-100   '
+              alt='asd'
+              layout='fill'
+            />
+          </a>
         </div>
       </div>
       <button
